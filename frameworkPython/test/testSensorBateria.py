1809c0c691483793f338ee7942654677daa7ff56
Diego Garcia <qtimpot@gmail.com>
2015-12-02 07:57:24 -0400

    creado tests Bateria

---
+++ frameworkPython/test/testSensorBateria.py
@@ -0,0 +1,50 @@
+import unittest
+from drivers.driver import Driver
+from hal.sensorBateria import SensorBateria
+
+
+data = {'nivel' : 100}
+status = 'ok'
+
+class DriverBateriaMock(Driver):
+
+    def __init__(self):
+
+        self.data = data
+        self.status = status
+
+    def getData(self):
+
+        return self.data
+
+    def getStatus(self):
+
+        return self.status
+
+    def forceRead(self):
+
+        raise NotImplementedError( "Should have implemented this" )
+
+    def reset(self):
+
+        raise NotImplementedError( "Should have implemented this" )
+
+class SensorGPSTest(unittest.TestCase):
+
+    def setUp(self):
+
+        self.sensorBateria = SensorBateria(DriverBateriaMock())
+
+    def test_getLastInfo_NoNone(self):
+
+        self.assertIsNotNone(self.sensorBateria.getLastInfo())
+
+    def test_getEstado_NoNone(self):
+
+        self.assertIsNotNone(self.sensorBateria.getEstado())
+
+    def test_getNivel(self):
+
+        sensorBateria = self.sensorBateria
+        esperado = data['nivel']
+        self.assertEquals(sensorBateria.getNivel(), esperado)
