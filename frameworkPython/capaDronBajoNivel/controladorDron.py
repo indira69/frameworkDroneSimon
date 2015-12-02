818aff49a5dbd7844682f642326c38a887c8b320
Indira Camacho <agrofru@gmail.com>
2015-12-02 08:05:43 -0300

    implemetación clase abstracta ControladorDron

--- frameworkPython/capaDronBajoNivel/controladorDron.py
+++ frameworkPython/capaDronBajoNivel/controladorDron.py
@@ -1,6 +1,25 @@
-from hal.actuadorOpenPilot import ActuadorOpenPilot
+_autor_= "I.C.C."
+
+import abc
+#Abstract Base Class todo son objetos en python y derivan de abc
+
+from abc import ABCMeta
 
 class ControladorDron(object):
+    __metaclass__ = ABCMeta
 
-    def __init__(self):
-        self.actuador= ActuadorOpenPilot()
+    # encender equivale a decir armar motores en OP
+    @abc.abstractmethod
+    def encender(self):
+        # pone los datos que viene en forma de diccionario de la capa controlador Dron
+        raise NotImplementedError( "Should have implemented this" )
+
+    @abc.abstractmethod
+    def apagar(self):
+        # pone los datos que viene en forma de diccionario de la capa controlador Dron
+        raise NotImplementedError( "Should have implemented this" )
+
+    @abc.abstractmethod
+    def apagar(self):
+        # pone los datos que viene en forma de diccionario de la capa controlador Dron
+        raise NotImplementedError( "Should have implemented this" )
