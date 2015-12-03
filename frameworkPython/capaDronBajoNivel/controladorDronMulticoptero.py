8dad40bcee3a6eed9259fa70bb4459ecbead0c04
Indira Camacho <agrofru@gmail.com>
2015-12-03 09:38:06 -0300

    controlador con todos los metodos, pero incompleto falta determinar comportamiento de alto nivel, los comandos y unidades de movimiento

--- frameworkPython/capaDronBajoNivel/controladorDronMulticoptero.py
+++ frameworkPython/capaDronBajoNivel/controladorDronMulticoptero.py
@@ -7,9 +7,9 @@ from hal.sensorUltrasonido import SensorUltrasonido
 from hal.sensorMagnometro import SensorMagnometro
 from hal.sensorBateria import SensorBateria
 
-from hal.sensorGPS import SensorGPS
+from controladorDronVolador import ControladorDronVolador
 
-class ControladorDronMulticoptero(object):
+class ControladorDronMulticoptero(ControladorDronVolador):
 
     def __init__(self):
         self.actuadorOP= ActuadorOpenPilot()
@@ -25,3 +25,50 @@ class ControladorDronMulticoptero(object):
 
     def apagar(self):
         self.actuadorOP.apagar()
+
+    # giro lateral de la cabeza
+    def yaw(self, grados):
+        velocidad= grados*360/100
+        self.actuadorOP.setYaw(velocidad)
+
+    # giro lateral de la cabeza a la izquierda
+    def yaw_izquierda(self, grados):
+        velocidad=grados*360/100
+        self.actuadorOP.setYaw(velocidad)
+
+
+    # giro lateral de la cabeza a la derecha
+    def yaw_derecha(self, grados):
+        velocidad=grados*360/100
+        self.actuadorOP.setYaw(velocidad)
+
+
+    # elevar el dron  distancia estará a
+    def up(self, distancia):
+        distancia1=distancia*????
+        self.actuadorOP.setThrotle(distancia1)
+
+    # bajar el dron distancia
+    def down(self, distancia):
+        distancia1=distancia*?????
+        self.actuadorOP.setThrotle(distancia1)
+
+    # cabeceo - elevación de la cabeza
+    def pitch_arriba(self, grados):
+        velocidad=grados*360/100
+        self.actuadorOP.setPitch(velocidad)
+
+    # cabeceo - bajar la cabeza
+    def pitch_abajo(self, grados):
+        velocidad=grados*360/100
+        self.actuadorOP.setPitch(velocidad)
+
+    # giro lateral de costado a la derecha
+    def roll_derecha(self, grados):
+        velocidad=grados*360/100
+        self.actuadorOP.setRoll(velocidad)
+
+    # giro lateral de costado a la izquierda
+    def roll_izquierda(self, grados):
+        velocidad=grados*360/100
+        self.actuadorOP.setRoll(velocidad)
