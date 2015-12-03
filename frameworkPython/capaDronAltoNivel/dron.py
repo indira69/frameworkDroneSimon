e7aa72c8cb5401b18abc5e58f1f45fb34e97c8ec
Indira Camacho <agrofru@gmail.com>
2015-12-03 09:55:38 -0300

    merge capa bajo nivel con alto nivel

---
+++ frameworkPython/capaDronAltoNivel/dron.py
@@ -0,0 +1,23 @@
+import abc
+#Abstract Base Class todo son objetos en python y derivan de abc
+
+from abc import ABCMeta
+from capaDronBajoNivel import ControladorDronMulticoptero
+
+class Dron (object):
+    __metaclass__ = ABCMeta
+
+    def __init__(self,ControladorDronMulticoptero):
+        """
+        :type controladorDron: Driver
+        """
+
+        # este sera un diccionario
+        self.controladorDron = None
+        self.status = None
+        self.driver = None
+
+    @abc.abstractmethod
+    def setData(self):
+        # pone los datos que viene en forma de diccionario de la capa controlador Dron
+        raise NotImplementedError( "Should have implemented this" )
